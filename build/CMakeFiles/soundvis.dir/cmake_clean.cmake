c20b15d800f6be15ad795510df5abfce132f1f2b
Jake Aigner <001diskey@gmail.com>
2024-06-22 03:34:50 -0500

    Initial commit

---
+++ build/CMakeFiles/soundvis.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/soundvis.dir/src/main.cpp.o"
+  "CMakeFiles/soundvis.dir/src/main.cpp.o.d"
+  "soundvis"
+  "soundvis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/soundvis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/soundvis.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for soundvis.
+# This may be replaced when dependencies are built.
